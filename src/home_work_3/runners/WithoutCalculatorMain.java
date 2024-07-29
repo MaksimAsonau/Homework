36106202882e7df9354f2054bbef3cd2ba7abf3c
Maksim Asonau <zabungalilovo@gmail.com>
2024-07-29 22:37:04 +0300

    Fixed typo

--- src/home_work_3/runners/WithoutCalculatorMain.java
+++ src/home_work_3/runners/WithoutCalculatorMain.java
@@ -4,7 +4,7 @@ public class WithoutCalculatorMain {
     public static void main(String[] args) {
 
         // Вычисляем значение выражения:  4.1 + 15 * 7 + (28 / 5) ^ 2
-        double result1 = 28.0 / 50;
+        double result1 = 28.0 / 5;
         double result2 = result1 * result1;
         double result3 = 15 * 7;
         double result4 = 4.1 + result3;
